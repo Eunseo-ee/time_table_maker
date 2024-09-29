1109dc49dd58ae15f31d7892be8952c2f9611754
Eunseo-ee <2whwhwh@gmail.com>
2024-09-30 02:07:04 +0900

    .

--- TimeTable/src/main/java/com/example/TimeTable/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.timetable.repository;
-
-import com.example.timetable.model.Courses;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Courses, Long> {
-}
